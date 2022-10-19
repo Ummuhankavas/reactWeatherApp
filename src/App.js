6f3c11e7cc63ed0232b22e2dab4724ee531bc101
Ümmühan Kavas <ummuhankavas@gmail.com>
2022-10-19 11:50:10 +0200

    useState, onChange added

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
   return (
     <div>
       <Header />
-      <Footer />
       <Main />
+      <Footer />
+      
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import { useState } from "react";
+import React from 'react';
 
 const Main = () => {
+    const [searchText, setsearchText] = useState('');
+    const handleChange = (e)=> {
+        setsearchText(e.target.value);
+    };
+
+    const getWeatherDatafromApi = async()=> {
+        
+    }
+
   return (
-    <section className='ajax-section'>
+    <section className='main'>
         <form>
-            <input type="text" placeholder='Search for a city' autoFocus />
+            <input onChange={handleChange} type="text" placeholder='Search for a city' autoFocus />
             <button type='submit'>SUBMIT</button>
             <span className='msg'></span>
         </form>
